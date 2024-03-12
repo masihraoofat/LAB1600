8c6ca814660a9d1b3585b66dc86fffd9edb8db6a
masihraoofat <masih.raoofat@gmail.com>
2024-03-11 20:47:59 -0400

    GDB no error

--- chaine_de_caractere/GDB/main.s
+++ chaine_de_caractere/GDB/main.s
@@ -62,7 +62,7 @@ jmp n_c2
 
 r:
 movl %ecx, %eax
-mov1 $0, %edx   # initialiser %edx a 0 avant la div (modif1)
+xor %edx,%edx   # initialiser %edx a 0 avant la div (modif1)
 divl %ebx
 cmp $0x00, %eax
 jmp e_n
@@ -84,10 +84,3 @@ bye:
 popl %ebx
 popl %ebp
 ret
-
-
-# Q2.2.1
-#	- n_c : Permet de parcourir la chaine et de calculer la somme des valeurs ASCII
-#	- d : Permet de vérifier si une lettre est une majuscule ou une minuscule
-#	- r : Défini le reste de la division et vérifie si ce reste est égal à zéro
-#	- e_n : Défini le numérateur de la division. Il est utilisé si le reste n'est pas égal à zéro
